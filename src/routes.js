fe65d129c9a1fc499f4b170c1a478651cc824907
Matheus <matheusdamatag@gmail.com>
2023-06-04 21:49:26 -0300

    Project started

---
+++ src/routes.js
@@ -0,0 +1,48 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const taks = database.select('tasks')
+
+      return res.end(JSON.stringify(taks))
+    },
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description } = req.body
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: '',
+      }
+
+      database.insert('tasks', task)
+
+      return res.writeHead(201).end()
+    },
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      database.delete('tasks', id)
+
+      return res.writeHead(204).end()
+    },
+  },
+]
